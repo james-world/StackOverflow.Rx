bb24b9cfaaab8619aab86045bc01b5bfdad6c76c
james-world <github@whirly.demon.co.uk>
2013-12-22 17:31:17 +0000

    Project structure & CombineWithPreceding operator
    Set up basic project structure. Created portable StackOverflow.Rx.Linq
    project and .NET 4.5.1 Tests.StackOverflow.Rx.Linq. Testing with
    rx-testing and nunit

---
+++ StackOverflow.Rx/StackOverflow.Rx.Linq/CombineWithPreceding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace StackOverflow.Rx.Linq
+{
+    /// <summary>
+    /// This operator is based on the answer to
+    /// http://stackoverflow.com/questions/2820685/get-previous-element-in-iobservable-without-re-evaluating-the-sequence/16581526#16581526
+    /// </summary>
+    public static partial class ObservableExtensions
+    {
+        /// <summary>
+        /// Projects the current and preceding element of a source sequence into a new form with the selector.
+        /// </summary>
+        /// <typeparam name="TSource">The type of source.</typeparam>
+        /// <typeparam name="TResult">The type of result.</typeparam>
+        /// <param name="source">A sequence of elements to invoke a transform function on.</param>
+        /// <param name="selector">A transform function to apply to each source element and it's predecessor.</param>
+        /// <returns>An observable sequence in a new form.</returns>
+        /// <remarks>The first time the selector is called, a default value is passed for the preceding element.</remarks>
+        public static IObservable<TResult> CombineWithPreceding<TSource, TResult>(
+            this IObservable<TSource> source,
+            Func<TSource, TSource, TResult> selector)
+        {
+            return source.Scan(
+                Tuple.Create(default(TSource), default(TSource)),
+                (previous, current) => Tuple.Create(current, previous.Item1))
+                .Select(element => selector(element.Item1, element.Item2));
+        }
+    }
+}
---
+++ StackOverflow.Rx/Tests.StackOverflow.Rx.Linq/CombineWithPreceding.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Reactive.Testing;
+using NUnit.Framework;
+using StackOverflow.Rx.Linq;
+
+namespace Tests.StackOverflow.Rx.Linq
+{
+    public class CombineWithPreceding : ReactiveTest
+    {
+        [Test]
+        public void IsCorrect()
+        {
+            var scheduler = new TestScheduler();
+            var source = scheduler.CreateColdObservable(
+                OnNext(100, 1),
+                OnNext(200, 2),
+                OnNext(300, 3),
+                OnCompleted<int>(400));
+
+            var results = scheduler.CreateObserver<Tuple<int,int>>();
+
+            source.CombineWithPreceding(Tuple.Create).Subscribe(results);
+
+            scheduler.Start();
+
+            results.Messages.AssertEqual(
+                OnNext(100, Tuple.Create(1, 0)),
+                OnNext(200, Tuple.Create(2, 1)),
+                OnNext(300, Tuple.Create(3, 2)),
+                OnCompleted<Tuple<int,int>>(400));
+        }
+    }
+}
