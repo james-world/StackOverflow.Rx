4cfb5c1b0c5c0701bc3be56d52b02fb3b74a8618
james-world <github@whirly.demon.co.uk>
2014-01-19 02:21:13 +0000

    Spy extension

---
+++ StackOverflow.Rx/StackOverflow.Rx.Linq/Spy.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace StackOverflow.Rx.Linq
+{
+    public partial class ObservableExtensions
+    {
+        private static Action<string> _spyLoggingAction = msg => Debug.WriteLine(msg);
+
+        /// <summary>
+        /// Set the method Spy uses to log messages
+        /// </summary>
+        /// <param name="log"></param>
+        public static void SetSpyLoggingAction(Action<string> log)
+        {
+            _spyLoggingAction = log;
+        }
+
+        /// <summary>
+        /// The Spy operator is based on the answer to
+        /// http://stackoverflow.com/questions/20220755/how-can-i-see-what-my-reactive-extensions-query-is-doing
+        /// It outputs details logging on the progress of an Observable.
+        /// Use ObservableExtensions.SetSpyLoggingAction to provide a log method suitable
+        /// for your logging library.
+        /// </summary>
+        public static IObservable<TSource> Spy<TSource>(
+            this IObservable<TSource> source,
+            string name = null)
+        {
+            name = name ?? "IObservable<" + typeof (TSource).Name + ">";
+            _spyLoggingAction(name + ": Evaluated on Thread " + Environment.CurrentManagedThreadId);
+            return Observable.Create<TSource>(o =>
+            {
+                _spyLoggingAction(
+                    string.Format(
+                        "{0}: Subscription started on Thread {1}",
+                        name,
+                        Environment.CurrentManagedThreadId));
+                try
+                {
+                    var subscription = source
+                        .Do(x  => _spyLoggingAction(
+                            string.Format("{0}: OnNext({1}) on Thread {2}",
+                                          name,
+                                          x,
+                                          Environment.CurrentManagedThreadId)),
+                            ex => _spyLoggingAction(
+                                string.Format(
+                                    "{0}: OnError({1}) on Thread {2}",
+                                    name,
+                                    ex,
+                                    Environment.CurrentManagedThreadId)),
+                            () => _spyLoggingAction(
+                                string.Format(
+                                    "{0}: OnCompleted() on Thread {1}",
+                                    name,
+                                    Environment.CurrentManagedThreadId))
+                        )
+                        .Subscribe(o);
+                    return new CompositeDisposable(
+                        subscription,
+                        Disposable.Create(() => _spyLoggingAction(
+                            string.Format(
+                                    "{0}: Cleaned up on Thread {1}",
+                                    name,
+                                    Environment.CurrentManagedThreadId))));
+                }
+                finally
+                {
+                    _spyLoggingAction(
+                        string.Format(
+                            "{0}: Subscription complete on Thread {1}",
+                            name,
+                            Environment.CurrentManagedThreadId));
+                }
+            });
+        }
+
+    }
+}
---
+++ StackOverflow.Rx/Tests.StackOverflow.Rx.Linq/Spy.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Reactive.Testing;
+using NUnit.Framework;
+using StackOverflow.Rx.Linq;
+using ObservableExtensions = StackOverflow.Rx.Linq.ObservableExtensions;
+
+namespace Tests.StackOverflow.Rx.Linq
+{
+    public class Spy : ReactiveTest
+    {
+        private List<string> _spyOutput;
+        private readonly object _spyOutputGate = new object();
+        private TestScheduler _scheduler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _spyOutput = new List<string>();
+            _scheduler = new TestScheduler();
+            ObservableExtensions.SetSpyLoggingAction(s =>
+            {
+                lock (_spyOutputGate)
+                {
+                    _spyOutput.Add(s);
+                }
+            });
+        }
+
+        [Test]
+        public void DefaultNameIsCorrect()
+        {
+            var source = Observable.Never<int>();
+
+            source.Spy();
+
+            Assert.IsTrue(_spyOutput[0].StartsWith("IObservable<Int32>:"));
+        }
+
+        [Test]
+        public void CustomNameIsCorrect()
+        {
+            var source = Observable.Never<int>();
+
+            source.Spy("Test");
+
+            Assert.IsTrue(_spyOutput[0].StartsWith("Test:"));
+        }
+
+        [Test]
+        public void EvaluationIsLogged()
+        {
+            var source = Observable.Never<int>();
+            const string evaluated = @"Test: Evaluated on Thread \d+";
+
+            source.Spy("Test");
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[0], evaluated));
+        }
+
+        [Test]
+        public void BeginSubscriptionMessageIsLogged()
+        {
+            var source = Observable.Never<int>();
+            const string beginSubscribe = @"Test: Subscription started on Thread \d+";
+
+            source.Spy("Test").Subscribe();
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[1], beginSubscribe));
+        }
+
+        [Test]
+        public void EndSubscriptionMessageIsLogged()
+        {
+            var source = Observable.Never<int>();
+            const string endSubscribe = @"Test: Subscription complete on Thread \d+";
+
+            source.Spy("Test").Subscribe();
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[2], endSubscribe));
+        }
+
+        [Test]
+        public void OnNextIsLogged()
+        {
+            var source = _scheduler.CreateColdObservable(
+                OnNext(100, 1));
+            var results = _scheduler.CreateObserver<int>();
+            const string expected = @"Test: OnNext\(1\) on Thread \d+";
+
+            source.Spy("Test").Subscribe(results);
+
+            _scheduler.Start();
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[3], expected));
+        }
+
+        [Test]
+        public void OnErrorIsLogged()
+        {
+            var source = _scheduler.CreateColdObservable(
+                OnError<int>(100, new Exception("Test Error")));
+            var results = _scheduler.CreateObserver<int>();
+            const string expected = @"Test: OnError\(System.Exception: Test Error\) on Thread \d+";
+
+            source.Spy("Test").Subscribe(results);
+
+            _scheduler.Start();
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[3], expected));
+        }
+
+        [Test]
+        public void OnCompletedIsLogged()
+        {
+            var source = _scheduler.CreateColdObservable(
+                OnCompleted<int>(100));
+            var results = _scheduler.CreateObserver<int>();
+            const string expected = @"Test: OnCompleted\(\) on Thread \d+";
+
+            source.Spy("Test").Subscribe(results);
+
+            _scheduler.Start();
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[3], expected));
+        }
+
+        [Test]
+        public void CleanUpIsLogged()
+        {
+            var source = _scheduler.CreateColdObservable(
+                OnCompleted<int>(100));
+            var results = _scheduler.CreateObserver<int>();
+            const string expected = @"Test: Cleaned up on Thread \d+";
+
+            source.Spy("Test").Subscribe(results);
+
+            _scheduler.Start();
+
+            Assert.IsTrue(Regex.IsMatch(_spyOutput[4], expected));
+        }
+    }
+}
